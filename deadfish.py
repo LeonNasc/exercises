0d34abd841ac82bddcd5d03c527fd2bca104c606
Leon Nascimento <l.nasc@live.com>
2018-09-11 12:24:49 -0300

    we did it!

--- deadfish.py
+++ deadfish.py
@@ -10,6 +10,33 @@ Invalid characters should be ignored.
 
 parse("iiisdoso") => [ 8, 64 ] '''
 
+def ParseInput(deadfish_i):
+    #parseia uma entrada deadfish
+    i=0
+    saida=[]
+    for op in deadfish_i:
+        if op == 'i':
+          i = incrementa(i)
+        elif op == 'd':
+          i =  decrementa(i)
+        elif  op == 's':
+          i = quadrado(i)
+        elif op == 'o':
+           printar(i,saida)
 
+    print(saida)
 
+def incrementa(num):
+    return num+1
 
+def decrementa(num):
+    return num-1
+
+def quadrado(num):
+    return num*num
+
+def printar(num,lista):
+    return lista.append(num)
+
+input="iiisdososssso"
+ParseInput(input)
