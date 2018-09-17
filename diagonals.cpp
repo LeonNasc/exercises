97dc6a3800c433b19fba5a5b496e982aa54fa0e7
Leon de França Nascimento <l.nasc@live.com>
2018-09-17 11:14:23 +0000

    Exercicios novos

---
+++ diagonals.cpp
@@ -0,0 +1,9 @@
+/* Given a "square" array of subarrays, find the sum of values from the first value of the first array, the second value of the second array, the third value of the third array, and so on...
+
+Example:
+
+var exampleArray = [[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]]
+ diagonalSum(exampleArray) // => 4 */
